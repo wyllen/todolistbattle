28082b9282b4c0099b610e035efa729a010e9c21
Vincent <vincent@Vincent-Durand-YX0N0FHV4D.local>
2022-12-19 16:42:00 +0100

    My amazing Todolist

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,32 +1,151 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [input, setInput] = useState('');
+
+  const handleAddTask = (e:any) => {
+    if(e.code === "Enter" && input !== "" ) {
+      setTasks(
+        (oldTasks :any) => {
+          const updatedTasks =  [...oldTasks];
+          updatedTasks.push({
+            label: input,
+            done: false,
+            category: currentCateg
+          });
+          return updatedTasks;
+        }
+      )
+      setInput("")
+    }
+  }
+
+
+  const [inputCategory, setInputCategory] = useState('');
+  
+  const [categories, setCategories] = useState<string[]>([]);
+
+  const [currentCateg, setcurrentCateg] = useState('');
+
+  const handleAddCategory = (e:any) => {
+    if(e.code === "Enter" && inputCategory !== "" ) {
+      setCategories(
+        (oldCategories :any) => {
+          const newCategs = [...oldCategories];
+          newCategs.push(inputCategory);
+          return newCategs;
+        }
+      )
+      setInputCategory("")
+    }
+  }
+
+
+  const initTasks:task[] = [
+    {
+      label:  "Ma super tache !!!",
+      category: "Football",
+      done: true,
+    },
+    {
+      label: "Mon autre tache !",
+      category: "Football",
+      done: false,
+    }
+  ]
+  interface task{
+    label: string;
+    done:boolean;
+    category?: string;
+  }
+   const [tasks, setTasks] = useState<task[]>(initTasks);
+
+   const handleFinishTask = (taskKey:number) => {
+    setTasks(
+      (oldTasks :any) => {
+        return [...oldTasks].map((task, key) => {
+          if (taskKey === key){
+            return {
+                ...task,
+                done: !task.done
+              }
+          }
+          return task
+        })
+      }
+    )
+   }
+
+   const handleSelectCateg = (e) => {
+    setcurrentCateg(e.target.value) 
+   }
+
+   useEffect(() => {
+      localStorage.setItem("tasks", JSON.stringify(tasks) )
+      localStorage.setItem("categories", JSON.stringify(categories) )
+      console.log(JSON.parse(localStorage.getItem("tasks") || ""))
+   }, [tasks, categories]);
+
+   useEffect(() => {
+    console.log(JSON.parse(localStorage.getItem("tasks") || ""))
+    setTasks(JSON.parse(localStorage.getItem("tasks") || ""))
+    setCategories(JSON.parse(localStorage.getItem("categories") || ""))
+ }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App todolist">
+      <div className="todolist__wrapper">
+      <div className="todolist__bg">
+        <h1>Toolot</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="todolist__tasks">
+        <h2>Tasks</h2>
+          <div className="todolist__tasks-form">
+            <input type="text" className="todolist_form-input" onChange={(e) => setInput(e.target.value)}  value={input} onKeyDown={handleAddTask}/>
+            <select onChange={handleSelectCateg}  className="todolist_form-select">
+              <option value=""></option>
+          {
+            categories?.map((categ) =>  <option selected={currentCateg === categ} value={categ}>{categ}</option>)
+          }</select>
+          </div>
+          <div className="todolist__tasks-list">
+              {
+            tasks.map((task, taskKey) =>  <div className={`todolist__tasks-list-item ${task.done ? "done" :""}`}>{task.label} - {task.category} :  <input  id={"c-"+taskKey}  type="checkbox" checked={task.done} onChange={() => handleFinishTask(taskKey)} /> <label for={"c-"+taskKey} className="todolist__tasks-list-item-check"></label></div>)
+          }
+          </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="todolist__categs">
+          <h2>Categories</h2>
+          <div className="todolist__categs-form">
+            <input type="text" onChange={(e) => setInputCategory(e.target.value)}  value={inputCategory} onKeyDown={handleAddCategory}/>
+          </div>
+          <div className="todolist__categs-list">{
+        categories?.map((categ) =>  <div className="todolist__categs-list-item">{categ}</div>)
+      }
+          </div>
+      </div>
+      </div>
+  {/*     <div>
+        <input type="text" onChange={(e) => setInput(e.target.value)}  value={input} onKeyDown={handleAddTask}/>
+        <select onChange={handleSelectCateg}>
+          <option value=""></option>
+      {
+        categories?.map((categ) =>  <option selected={currentCateg === categ} value={categ}>{categ}</option>)
+      }</select>
+      </div>
+      {
+        tasks.map((task, taskKey) =>  <div>{task.label} - {task.category} :  <input type="checkbox" checked={task.done} onChange={() => handleFinishTask(taskKey)} /></div>)
+      }
+
+      <div className="categories">
+        <input type="text" onChange={(e) => setInputCategory(e.target.value)}  value={inputCategory} onKeyDown={handleAddCategory}/>
+      {
+        categories?.map((categ) =>  <div>{categ}</div>)
+      }
+      </div> */}
     </div>
   )
 }
